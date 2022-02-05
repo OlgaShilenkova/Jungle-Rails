b006db919971127c5d7b22119ea546eea24d9354
Olga Shilenkova <Olga1shilenkova@gmail.com>
2022-02-04 21:49:14 -0600

    created routes for admin:: categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -6,4 +6,24 @@ class Admin::DashboardController < ApplicationController
     @categories = Category.all
     @allProducts = Product.all
   end
+
+  def index
+    @categories = Category.all
+    @allProducts = Product.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(:name)
+
+    if @category.save
+      redirect_to [:admin, :category], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
 end
